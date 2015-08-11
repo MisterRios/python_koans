ff8a5bba5645bf958c921af9a3dbe3efc92a3d80
Daniel A Rios <misterrios@gmail.com>
2015-08-11 20:17:41 +0200

    finished Triangle 2

--- python3/koans/triangle.py
+++ python3/koans/triangle.py
@@ -18,6 +18,11 @@
 #
 def triangle(a, b, c):
     # DELETE 'PASS' AND WRITE THIS CODE
+    if a < 1 or b < 1 or c < 1:
+    	raise TriangleError
+    if (a + b < c) or (a + c < b) or (b + c < a):
+    	raise TriangleError
+
     if a == b == c:
     	return 'equilateral'
     elif a == b or b == c or c == a:
